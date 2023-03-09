282ac98221f07baddf4736280cc8ecd477a4b661
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-03-09 16:00:36 +0000

    Add basic functions and event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+const cardOne = document.querySelector('.card-1')
+const cardTwo = document.querySelector('.card-2')
+const cardThree = document.querySelector('.card-3')
+const cardFour = document.querySelector('.card-4')
+const cardFive = document.querySelector('.card-5')
+
+document.getElementById('hi-button').addEventListener('click', higherCard);
+document.getElementById('low-button').addEventListener('click', lowerCard);
+document.getElementById('start-button').addEventListener('click', startGame);
+document.getElementById('restart-button').addEventListener('click', restartGame);
+document.getElementById('current-score').innerText ="";
+document.getElementById('high-score').innerText ="";
+document.getElementById('text').innerText ="";
+
+function restartGame(){}
+function startGame(){}
+function newGame(){}
+function enableButtons(){}
+function buttonDisable(){}
+function higherCard(){}
+function lowerCard(){}
+function higherCardCheck(){}
+function lowerCardCheck(){}
+function randomCard(){}
+function Deck(){}
+function shuffle(){}
