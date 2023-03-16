102224fb7352706aec095949f985981fc0e83341
ACroshaw88 <andrewcroshaw88@outlook.com>
2023-03-16 11:35:03 +0000

    Add Comments to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 import newDeck from "./card-deck.js";
 
+//global variable array to map the card values
 const cardValue = {
     "2": 2,
     "3": 3,
@@ -16,37 +17,46 @@ const cardValue = {
     A: 14
 };
 
+//global variables to hold card div elements to allow for showing of the cards
 const cardOne = document.querySelector('.card-1');
 const cardTwo = document.querySelector('.card-2');
 const cardThree = document.querySelector('.card-3');
 const cardFour = document.querySelector('.card-4');
 const cardFive = document.querySelector('.card-5');
+
+//global variable to hold game text 
 const text = document.getElementById('text');
+
+//global variable to count cards
 let cardCounter = 0;
+
+// global variables to hold items entries for the card entries and rule text
 let cardSlotOne, cardSlotTwo, cardSlotThree, cardSlotFour, cardSlotFive, ruleText;
+
+//global array to hold random selected cards 
 let items = [];
+
+// global variables to hold scores
 let scoreCount = 0;
 let highScoreCount = 0;
 
-document.getElementById('hi-button').style.visibility = 'hidden';
-document.getElementById('low-button').style.visibility = 'hidden';
+//button event listeners 
 document.getElementById('hi-button').addEventListener('click', higherCard);
 document.getElementById('low-button').addEventListener('click', lowerCard);
 document.getElementById('start-button').addEventListener('click', startGame);
 document.getElementById('play-btn').addEventListener('click', hideRules);
 document.getElementById('how-to-play').addEventListener('click', howToPlay);
 document.getElementById('rule-btn').addEventListener('click', showRules);
-document.getElementById('current-score').innerText = "0";
-document.getElementById("high-score").innerText = "0";
-text.innerText = "Press Start To Begin";
 
+/**
+ * resets game elements for a new game
+ */
 function startGame() {
     cardOne.innerHTML = "";
     cardTwo.innerHTML = "";
     cardThree.innerHTML = "";
     cardFour.innerHTML = "";
     cardFive.innerHTML = "";
-    document.getElementById('text').innerText = "Press Start To Begin";
     cardCounter = 0;
     scoreCount = 0;
     document.getElementById('current-score').innerText = "0";
@@ -57,6 +67,12 @@ function startGame() {
     ShowLowHightButtons();
 }
 
+/**
+ * creats a new deck of cards
+ * suffles the new deck
+ * randomly selects 15 cards and places them in to items array
+ * replaces innerText on start button to restart
+ */
 function newGame() {
     document.getElementById("start-button").innerText = "RESTART";
     let deck = new newDeck();
@@ -72,16 +88,11 @@ function newGame() {
     cardSlotFive = items[4];
 }
 
-function hideLowHightButtons() {
-    document.getElementById('hi-button').style.visibility = 'hidden';
-    document.getElementById('low-button').style.visibility = 'hidden';
-}
-
-function ShowLowHightButtons() {
-    document.getElementById('hi-button').style.visibility = 'visible';
-    document.getElementById('low-button').style.visibility = 'visible';
-}
-
+/**
+ * Shows game the game rules element
+ * resets the restart button text to start
+ * resets the card elements
+ */
 function showRules() {
     document.getElementById("game-rules").style.visibility = 'visible';
     document.getElementById("start-button").innerText = "START";
@@ -90,17 +101,22 @@ function showRules() {
     cardThree.innerHTML = "";
     cardFour.innerHTML = "";
     cardFive.innerHTML = "";
-
 }
 
-howToPlay();
-
+// hide the game rules element
 function hideRules() {
     document.getElementById("game-rules").style.visibility = 'hidden';
 }
 
-function howToPlay() {
+/**
+ * renders innertext to the game rules element depending on which button is pressed
+ * this uses the ruleText variable to determin which set to display
+ * @param{let} ruleText
+ */
 
+howToPlay();
+
+function howToPlay() {
     if (ruleText === 0) {
         document.getElementById('rule-header').innerText = "How To Play";
         document.getElementById('rule-text-1').innerText = "Press the Start button to begin a game";
@@ -121,12 +137,30 @@ function howToPlay() {
     }
 }
 
+/**
+ * for high card checks
+ * @var{let} cardCount Check to run which checks needs to be made
+ * @var{let} cardSlotOne
+ * @var{let} cardSlotTwo
+ * @var{let} cardSlotThree
+ * @var{let} cardSlotFour
+ * @var{let} cardSlotFive
+ * @function cardOneTwoCheck checks answer on first and second cards
+ * @function cardTwoThreeCheck checks answer on second and third cards
+ * @function cardThreeFourCheck checks answer on third and fourth cards
+ * @function cardFourFiveCheck checks answer on fourth and fifth cards
+ * @function incorrect to display incorrect items to page
+ * @function cardSetTwo to collect next cards to be shown
+ * @function cardSetThree to collect next cards to be shown
+ * @function higherCardCheck checks the two card values
+ */
 function higherCard() {
     if (cardCounter === 1) {
         if (higherCardCheck(cardSlotOne, cardSlotTwo)) {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 2) {
@@ -134,6 +168,7 @@ function higherCard() {
             cardTwoThreeCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardThree.appendChild(cardSlotThree.cardHTML());
         }
     } else if (cardCounter === 3) {
@@ -141,6 +176,7 @@ function higherCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
     } else if (cardCounter === 4) {
@@ -149,6 +185,7 @@ function higherCard() {
             cardSetTwo();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
         }
     } else if (cardCounter === 5) {
@@ -156,6 +193,7 @@ function higherCard() {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 6) {
@@ -163,6 +201,7 @@ function higherCard() {
             cardTwoThreeCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardThree.appendChild(cardSlotThree.cardHTML());
         }
     } else if (cardCounter === 7) {
@@ -170,6 +209,7 @@ function higherCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
     } else if (cardCounter === 8) {
@@ -178,6 +218,7 @@ function higherCard() {
             cardSetThree();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
         }
     } else if (cardCounter === 9) {
@@ -185,6 +226,7 @@ function higherCard() {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 10) {
@@ -199,15 +241,18 @@ function higherCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
     } else if (cardCounter === 12) {
         if (higherCardCheck(cardSlotFour, cardSlotFive)) {
             cardFourFiveCheck();
-            text.innerText = "Congratulations You Win";
+            //shows winning text to game text area
+            text.innerText = "Congratulations You Win 🏆";
             hideLowHightButtons();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
 
         }
@@ -216,12 +261,30 @@ function higherCard() {
     }
 }
 
+/**
+ * for low card checks
+ * @var{let} cardCount Check to run which checks needs to be made
+ * @var{let} cardSlotOne stores items index 0
+ * @var{let} cardSlotTwo stores items index 2
+ * @var{let} cardSlotThree stores items index 3
+ * @var{let} cardSlotFour stores items index 4
+ * @var{let} cardSlotFive stores items index 5
+ * @function cardOneTwoCheck checks answer on first and second cards
+ * @function cardTwoThreeCheck checks answer on second and third cards
+ * @function cardThreeFourCheck checks answer on third and fourth cards
+ * @function cardFourFiveCheck checks answer on fourth and fifth cards
+ * @function incorrect to display incorrect items to page
+ * @function cardSetTwo to collect next cards to be shown
+ * @function cardSetThree to collect next cards to be shown
+ * @function LowerCardCheck checks the two card values
+ */
 function lowerCard() {
     if (cardCounter === 1) {
         if (lowerCardCheck(cardSlotOne, cardSlotTwo)) {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 2) {
@@ -229,6 +292,7 @@ function lowerCard() {
             cardTwoThreeCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardThree.appendChild(cardSlotThree.cardHTML());
         }
     } else if (cardCounter === 3) {
@@ -236,15 +300,16 @@ function lowerCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
-
     } else if (cardCounter === 4) {
         if (lowerCardCheck(cardSlotFour, cardSlotFive)) {
             cardFourFiveCheck();
             cardSetTwo();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
         }
     } else if (cardCounter === 5) {
@@ -252,6 +317,7 @@ function lowerCard() {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 6) {
@@ -259,6 +325,7 @@ function lowerCard() {
             cardTwoThreeCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardThree.appendChild(cardSlotThree.cardHTML());
         }
     } else if (cardCounter === 7) {
@@ -266,6 +333,7 @@ function lowerCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
     } else if (cardCounter === 8) {
@@ -274,6 +342,7 @@ function lowerCard() {
             cardSetThree();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
         }
     } else if (cardCounter === 9) {
@@ -281,6 +350,7 @@ function lowerCard() {
             cardOneTwoCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardTwo.appendChild(cardSlotTwo.cardHTML());
         }
     } else if (cardCounter === 10) {
@@ -288,6 +358,7 @@ function lowerCard() {
             cardTwoThreeCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardThree.appendChild(cardSlotThree.cardHTML());
         }
     } else if (cardCounter === 11) {
@@ -295,15 +366,18 @@ function lowerCard() {
             cardThreeFourCheck();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFour.appendChild(cardSlotFour.cardHTML());
         }
     } else if (cardCounter === 12) {
         if (lowerCardCheck(cardSlotFour, cardSlotFive)) {
             cardFourFiveCheck();
-            text.innerText = "Congratulations You Win";
+            //shows winning text to game text area
+            text.innerText = "Congratulations You Win 🏆";
             hideLowHightButtons();
         } else {
             incorrect();
+            //shows card after wrong answer
             cardFive.appendChild(cardSlotFive.cardHTML());
         }
     } else {
@@ -311,11 +385,39 @@ function lowerCard() {
     }
 }
 
+// hides hi and low buttons
+function hideLowHightButtons() {
+    document.getElementById('hi-button').style.visibility = 'hidden';
+    document.getElementById('low-button').style.visibility = 'hidden';
+}
+
+//shows the high and low buttons
+function ShowLowHightButtons() {
+    document.getElementById('hi-button').style.visibility = 'visible';
+    document.getElementById('low-button').style.visibility = 'visible';
+}
+
+//changes game text area and hides hi and low buttons on incorrect guess
 function incorrect() {
     text.innerText = "Sorry You Lost";
     hideLowHightButtons();
 }
 
+/**
+ * replace cardSlot variables with a new index for next set of cards 
+ * @var{let} cardSlotOne stores items index 4
+ * @var{let} cardSlotTwo stores items index 5
+ * @var{let} cardSlotThree stores items index 6
+ * @var{let} cardSlotFour stores items index 7
+ * @var{let} cardSlotFive stores items index 8
+ * clears cardOne - cardFive variables in html to start the next cards in the game
+ * then shows new cardOne item using innetHTML
+ * @var{let} cardOne 
+ * @var{let} cardTwo 
+ * @var{let} cardThree 
+ * @var{let} cardFour 
+ * @var{let} cardFive 
+ */
 function cardSetTwo() {
     cardSlotOne = items[4];
     cardSlotTwo = items[5];
@@ -330,6 +432,21 @@ function cardSetTwo() {
     cardOne.appendChild(cardSlotOne.cardHTML());
 }
 
+/**
+ * replace cardSlot variables with a new index for next set of cards 
+ * @var{let} cardSlotOne stores items index 4
+ * @var{let} cardSlotTwo stores items index 5
+ * @var{let} cardSlotThree stores items index 6
+ * @var{let} cardSlotFour stores items index 7
+ * @var{let} cardSlotFive stores items index 8
+ * clears cardOne - cardFive variables in html to start the next cards in the game
+ * then shows new cardOne item using innetHTML
+ * @var{let} cardOne 
+ * @var{let} cardTwo 
+ * @var{let} cardThree 
+ * @var{let} cardFour 
+ * @var{let} cardFive 
+ */
 function cardSetThree() {
     cardSlotOne = items[8];
     cardSlotTwo = items[9];
@@ -344,38 +461,87 @@ function cardSetThree() {
     cardOne.appendChild(cardSlotOne.cardHTML());
 }
 
+/**
+ * function to run if card guess four five is correct
+ * @var{let} cardSlotFive stores items index 8
+ * @var{let} cardFive 
+ * @function score
+ **/
 function cardFourFiveCheck() {
+    // shows card five place ment
     cardFive.appendChild(cardSlotFive.cardHTML());
+    //adds one to the card counter
     cardCounter++;
+    //runs score function
     score();
 }
 
+/**
+ * function to run if card guess Three four is correct
+ * @var{let} cardSlotFour
+ * @var{let} cardFour 
+ * @function score
+ **/
 function cardThreeFourCheck() {
     cardFour.appendChild(cardSlotFour.cardHTML());
+    //adds one to the card counter
     cardCounter++;
+    //runs score function
     score();
 }
 
+/**
+ * function to run if card guess Two Three is correct
+ * @var{let} cardSlotThree
+ * @var{let} cardThree
+ * @function score
+ **/
 function cardTwoThreeCheck() {
     cardThree.appendChild(cardSlotThree.cardHTML());
+    //adds one to the card counter
     cardCounter++;
+    //runs score function
     score();
 }
 
+/**
+ * function to run if card guess One Two is correct
+ * @var{let} cardSlotTwo
+ * @var{let} cardTwo
+ * @function score
+ **/
 function cardOneTwoCheck() {
     cardTwo.appendChild(cardSlotTwo.cardHTML());
+    //adds one to the card counter
     cardCounter++;
+    //runs score function
     score();
 }
 
+/**
+ * takes the value of the cardSlot variables and compares it with the map values and returns which card is higher
+ * @param {*} cardOne 
+ * @param {*} cardTwo 
+ * @returns 
+ */
 function higherCardCheck(cardOne, cardTwo) {
     return cardValue[cardOne.value] <= cardValue[cardTwo.value];
 }
 
+/**
+ * takes the value of the cardSlot variables and compares it with the map values and returns which card is lower
+ * @param {*} cardOne 
+ * @param {*} cardTwo 
+ * @returns 
+ */
 function lowerCardCheck(cardOne, cardTwo) {
     return cardValue[cardOne.value] >= cardValue[cardTwo.value];
 }
 
+/**
+ * adds then to score count on a correct guess
+ * checks highScoreCount against scoreCount and if scoreCount is greater implements new high score
+ */
 function score() {
     scoreCount = scoreCount + 10;
     document.getElementById("current-score").innerText = scoreCount;
